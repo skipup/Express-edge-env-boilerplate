5180de0b072e2289185db0eed44af9ca8c410ca1
skipup <skipup123$>
2020-07-29 15:01:47 +0630

    👺

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-import express from 'express';
-import Router from './router';
-import {config,engine} from 'express-edge';
-import mongoose, { mongo } from 'mongoose';
+import express from "express";
+import Router from "./router";
+import { config, engine } from "express-edge";
+import mongoose, { mongo } from "mongoose";
 const app = express();
 
 //## setup dotenv
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 //## Setting up mongodb
-mongoose.connect(process.env.mongo_url,{useNewUrlParser:true,useUnifiedTopology:true},(e,client)=>{
-    if(e) return e;
-    console.log('Mongo Connection Success')
-})
+mongoose.connect(
+  process.env.mongo_url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (e, client) => {
+    if (e) return e;
+    console.log("Mongo Connection Success");
+  }
+);
 
 //## Static File
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + "/public"));
 
 //##    Setup edge template engine
-app.use(engine)
-app.set('views',__dirname+'/views')
+app.use(engine);
+app.set("views", __dirname + "/views");
 
 //##    routing
-app.use(Router)
-
+app.use(Router);
 
 //##  setting Server
 const port = process.env.port;
-app.listen(port,()=>{
-    console.log('server running on port'+port);
-})
+app.listen(port, () => {
+  console.log("server running on port" + port);
+});
